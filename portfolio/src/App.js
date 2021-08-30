a0848ebab879472dfb94cd3a0b2a4dff0ad218bb
Steph G <61924023+sbgastondesign@users.noreply.github.com>
2021-08-30 16:45:59 -0400

    udapted files, footer added,  linking styles

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,10 +3,11 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Footer from "./components/Footer";
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Navbar />
       <Header />
       <Section />
