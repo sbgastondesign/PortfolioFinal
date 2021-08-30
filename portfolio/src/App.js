aa24d97caa80955e09710c1946825835135b5ddd
Steph G <61924023+sbgastondesign@users.noreply.github.com>
2021-08-30 16:21:43 -0400

    styling and updates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,22 +1,16 @@
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Header />
+      <Section />
+      <Footer />
     </div>
   );
 }
